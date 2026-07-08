577cbc26321d73aaedcfa2d4e6c695f9ca2d0f8a
timzy <timzy@TIMMY>
2026-07-08 11:04:09 +0100

    Add project files.

---
+++ BEGINNER PROJECT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+String userChoice = Console.ReadLine();
+Console.WriteLine("User input:" + userChoice);
+userChoice = "ABC"; 
+
+Console.ReadKey();
